b9703c3ca5cc3fef70fd6116627a51237a41e823
JunePhone <672315226@qq.com>
2019-09-16 18:20:13 +0800

    final version React

---
+++ config/webpack.base.conf.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const  HappyPack  = require('happypack');
+const { HappyThreadPool } = require('happypack');
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        filename: 'bundle.[hash].js',
+        path: path.join(__dirname, '/dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+                include: path.resolve(__dirname, 'src')
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(gif|jpg|png|bmp|eot|woff|woff2|ttf|svg)/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            outputPath: 'images'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.less/,
+                use: ['style-loader', 'css-loader', 'less-loader'],
+                exclude: /node_modules/,
+                include: path.resolve(__dirname, 'src')
+            },
+            {
+                test: /\.scss/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+                exclude: /node_modules/,
+                include: path.resolve(__dirname, 'src')
+            },
+            // {test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/},
+              {
+                test: /\.jsx?$/,
+                use: [
+                    {
+                        // 一个loader对应一个id
+                        loader: "happypack/loader?id=JSX"
+                    }
+                ],
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new CleanWebpackPlugin(),
+        new HappyPack({
+            // 用唯一的标识符id，来代表当前的HappyPack是用来处理一类特定的文件
+            id:'JSX',
+            // 如何处理.js文件，用法和Loader配置中一样
+            loaders:['babel-loader?cacheDirectory']
+        })
+    ]
+};
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import {CssBaseline} from "@material-ui/core";
+class App extends React.Component {
+    render() {
+        return( <React.Fragment>
+            <CssBaseline />
+            hello!React
+        </React.Fragment>)
+    }
+}
+
+export default App;
